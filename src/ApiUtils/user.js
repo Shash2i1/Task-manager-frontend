641abce4bbc21b37477076c38644cfddf6187ab8
Shash2i1 <shashanknaribole3@gmail.com>
2025-03-05 13:19:42 +0530

    env removed

--- src/ApiUtils/user.js
+++ src/ApiUtils/user.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
+const API_BASE_URL = "https://task-manager-backend-lizm.onrender.com"
 export class UserFunctions{
 
     //Method fro register user
