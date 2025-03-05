e70f39f8f0b3da16980a712611642b787a1f7826
Shash2i1 <shashanknaribole3@gmail.com>
2025-03-05 13:13:22 +0530

    env added

--- src/ApiUtils/user.js
+++ src/ApiUtils/user.js
@@ -1,11 +1,11 @@
 import axios from "axios"
-
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
 export class UserFunctions{
 
     //Method fro register user
     async registerUser({userName, email, password}){
         try {
-            const response = await axios.post("/api/v1/user/register",{userName,email,password})
+            const response = await axios.post(`${API_BASE_URL}/api/v1/user/register`,{userName,email,password})
             return response.data
         } catch (error) {
             console.log("Error while registering || registerUser",error)
@@ -16,7 +16,7 @@ export class UserFunctions{
     //Method for login the user
     async loginUser({email, password}){
         try {
-            const response = await axios.post("/api/v1/user/login",{email, password});
+            const response = await axios.post(`${API_BASE_URL}/api/v1/user/login`,{email, password});
 
             return response.data
         } catch (error) {
@@ -28,7 +28,7 @@ export class UserFunctions{
     //method to get current user details
     async getCurrentUser(){
         try {
-            const response = await axios.get("/api/v1/user/get-current-user")
+            const response = await axios.get(`${API_BASE_URL}/api/v1/user/get-current-user`)
             return response.data;
         } catch (error) {
             console.log("Error While fetching user|| getCurrentUser",error)
@@ -38,7 +38,7 @@ export class UserFunctions{
     //method to logout the user
     async logoutUser(){
         try {
-            await axios.post("/api/v1/user/logout");
+            await axios.post(`${API_BASE_URL}/api/v1/user/logout`);
         } catch (error) {
             console.log("Error while logout user|| logoutUser",error);
         }
--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server:{
-    proxy:{
+    /*proxy:{
       '/api/v1': {
         target: 'https://task-manager-backend-lizm.onrender.com',
         changeOrigin: true,
         secure: false,
       },
-    }
+    }*/
   },
   plugins: [react()],
 })
